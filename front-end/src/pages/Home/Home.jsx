8aba606d07d14f907089f7aade2a43e7634abfb4
GiovanniBolivoro <101050472+GiovanniEliasDaRosa@users.noreply.github.com>
2026-04-14 11:14:38 -0300

    fix(header): use links for header buttons
    - Make header buttons be a NavLink, now working when you click them
    - Add logic to change the buttons depending on auth state:
      - If no auth, 2 buttons show up "register" and "login"
      - If has auth, 2 buttons show up "home" and "logout"
    - Make the header text be a paragraph instead of a h1 to be more semantic

--- front-end/src/pages/Home/Home.jsx
+++ front-end/src/pages/Home/Home.jsx
@@ -1,19 +1,7 @@
-import { useContext } from "react";
-import { AuthContext } from "../../contexts/AuthContext";
-import { useNavigate } from "react-router";
-
 export default function Home() {
-  const { logout } = useContext(AuthContext);
-  const navigate = useNavigate();
-
-  function handleLogout() {
-    logout();
-    navigate("/login");
-  }
-
   return (
     <div>
-      <button onClick={handleLogout}>Log out</button>
+      <h1>Home</h1>
     </div>
   );
 }
