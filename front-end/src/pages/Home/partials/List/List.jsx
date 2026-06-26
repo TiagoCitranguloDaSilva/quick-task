532c24ed74d75fa0c284e113424dc68b668b2f16
Giovanni <101050472+GiovanniEliasDaRosa@users.noreply.github.com>
2026-06-26 14:43:21 -0300

    fix(list): improve list visuals

--- front-end/src/pages/Home/partials/List/List.jsx
+++ front-end/src/pages/Home/partials/List/List.jsx
@@ -3,22 +3,24 @@ import { Link } from "react-router";
 import styles from "./List.module.css";
 
 export default function List({ list }) {
-  console.log(list);
-
   const listTasksLimited = list.tasks.slice(0, 4);
 
   return (
     <Link to={`/list/${list.id}`} className={styles.list} data-no-default="true">
-      <p className={styles.list_tittle}>{list.title}</p>
+      <p className={styles.list_title}>{list.title}</p>
       <p className={styles.list_description}>{list.description}</p>
 
       <div className={styles.list_items}>
-        {listTasksLimited?.map((task) => (
-          <span className={styles.list_item} key={`list_${list.id}_item_${task.id}`}>
-            <input type="checkbox" />
-            <span>{task.content}</span>
-          </span>
-        ))}
+        {listTasksLimited?.map((task) => {
+          const inputId = `task_${list.id}_${task.id}`;
+
+          return (
+            <div className={styles.list_item} key={inputId}>
+              <input type="checkbox" id={inputId} onClick={(e) => e.stopPropagation()} />
+              <label htmlFor={inputId}>{task.content}</label>
+            </div>
+          );
+        })}
       </div>
     </Link>
   );
