feaddf7f80965331e1a16981539f7b2d1280fcc5
Giovanni <101050472+GiovanniEliasDaRosa@users.noreply.github.com>
2026-07-01 11:32:01 -0300

    feat(lists): implement list deletion
    - Add backend request to delete the selected list

--- front-end/src/pages/Home/partials/DeleteList/DeleteList.jsx
+++ front-end/src/pages/Home/partials/DeleteList/DeleteList.jsx
@@ -1,19 +1,61 @@
-import { useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import styles from "./DeleteList.module.css";
 import { Trash2, TriangleAlert, X } from "lucide-react";
+import useApi from "../../../../hooks/useApi";
 
 export default function DeleteList({ ref, updateLists, currentListEditing }) {
+  const { fetchWithAuth } = useApi();
+
+  const controllerRef = useRef(new AbortController());
   const [requestStatus, setRequestStatus] = useState("idle");
+  const mountedRef = useRef(true);
 
   function close() {
     ref.current?.close();
   }
 
-  function handleSubmitButton() {
-    console.log(currentListEditing.title);
-    //
-    setRequestStatus("error");
-  }
+  const requestEditList = useCallback(async (data) => {
+    setRequestStatus("idle");
+
+    try {
+      const response = await fetchWithAuth(
+        `http://localhost:8080/list/delete/${currentListEditing.id}`,
+        {
+          method: "DELETE",
+          signal: controllerRef.current.signal,
+        },
+      );
+
+      if (!response.ok) {
+        setRequestStatus("error");
+        const text = await response.text();
+        throw new Error(`API error ${response.status}: ${text}`);
+      }
+
+      // With success on editing
+      setRequestStatus("success");
+
+      // Close dialog
+      ref.current?.close();
+
+      // Update lists on home
+      updateLists();
+    } catch (e) {
+      // If still on the same screen show error on screen
+      console.error(e);
+      setRequestStatus("error");
+    }
+  });
+
+  useEffect(() => {
+    mountedRef.current = true;
+
+    return () => {
+      mountedRef.current = false;
+      // On unmount cancel fetch
+      controllerRef.current.abort();
+    };
+  }, []);
 
   return (
     <dialog className="dialog" ref={ref}>
@@ -37,7 +79,7 @@ export default function DeleteList({ ref, updateLists, currentListEditing }) {
               Cancel
             </button>
 
-            <button className="submit_button destructive" onClick={handleSubmitButton}>
+            <button className="submit_button destructive" onClick={requestEditList}>
               <Trash2 />
               Delete
             </button>
