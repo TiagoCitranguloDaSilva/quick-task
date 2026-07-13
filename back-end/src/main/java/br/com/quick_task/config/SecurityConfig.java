86ed7989ee0a917bfbd64ce6285cf1b66d840fb3
tiago <tiagocitrangulo256@gmail.com>
2026-07-13 10:21:06 -0300

    feat: make swagger routes public
    - make all "/swagger-ui/**" public
    - make all "/v3/api-docs/**" public

--- back-end/src/main/java/br/com/quick_task/config/SecurityConfig.java
+++ back-end/src/main/java/br/com/quick_task/config/SecurityConfig.java
@@ -33,6 +33,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(request ->
                         request
                                 .requestMatchers("/auth/*").permitAll()
+                                .requestMatchers("/swagger-ui/**").permitAll()
+                                .requestMatchers("/v3/api-docs/**").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
